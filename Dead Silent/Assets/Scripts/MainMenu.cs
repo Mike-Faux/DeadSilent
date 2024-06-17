c4d36a506d0c0f5a1ec0af2228eee0e3556f9c20
Payson Allen <137624600+NoxisNG@users.noreply.github.com>
2024-06-17 12:23:48 -0500

    MainMenu

---
+++ Dead Silent/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Play()
+        {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
+        }
+
+    public void Quit()
+        {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
