a3fbb1488e5a278e6b7bd73d051f5be6e869d608
Dusk_Xua <mike@faux.technology>
2024-06-19 23:10:42 -0400

    Alpha Build

--- Dead Silent/Assets/Scripts/Maze/DrawCircleMaze.cs
+++ Dead Silent/Assets/Scripts/Maze/DrawCircleMaze.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.AI.Navigation;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.AI;
 
