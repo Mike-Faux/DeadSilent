dc82d3c231106f16669f24ff7813a5b29fc26c83
Payson Allen <137624600+NoxisNG@users.noreply.github.com>
2024-06-24 08:03:19 -0500

    Fixed player animations
    Simplified player animations to a different animation controller so that player movement does not interfere with reload or recoil animations

--- EternalPursuit/Assets/Imported assets/scripts/AnimationStateController.cs
+++ EternalPursuit/Assets/Imported assets/scripts/AnimationStateController.cs
@@ -15,7 +15,6 @@ public class AnimationStateController : MonoBehaviour
     int isReloadingHash;
     int RecoilHash;
 
-    // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -30,100 +29,48 @@ public class AnimationStateController : MonoBehaviour
         RecoilHash = Animator.StringToHash("Recoil");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        bool isWalkingBack = animator.GetBool(isWalkingBackHash);
-        bool isrunningBack = animator.GetBool(isRunningBackHash);
-        bool isrunning = animator.GetBool(isRunningHash);
-        bool isWalking = animator.GetBool(isWalkingHash);
-        bool isShooting = animator.GetBool(isShootingHash);
         bool forwardPressed = Input.GetKey("w");
         bool runPressed = Input.GetKey("left shift");
         bool leftPressed = Input.GetKey("a");
         bool rightPressed = Input.GetKey("d");
         bool backPressed = Input.GetKey("s");
-       
-        bool isReloading = animator.GetBool(isReloadingHash);
-        bool isshooting = Input.GetMouseButton(0);
 
+        bool isReloading = animator.GetBool(isReloadingHash);
+        bool isshooting = Input.GetButton("Fire1");
+
+        // Reload logic
         if (!isReloading && Input.GetKeyDown("r"))
         {
             animator.SetBool(isReloadingHash, true);
             StartCoroutine(ResetIsReloading());
-            
         }
 
-        IEnumerator ResetIsReloading()
-        {
-            // Assuming the reload animation is 2 seconds long
-            yield return new WaitForSeconds(2);
-            animator.SetBool(isReloadingHash, false);
-        }
-
-        if (!isShooting && isshooting)
+        // Shooting logic, allowing shooting during movement but not during reloading
+        if (!isReloading && isshooting)
         {
             animator.SetBool(isShootingHash, true);
             animator.SetBool(RecoilHash, true);
         }
-        if (isShooting && !isshooting)
+        else if (!isshooting)
         {
             animator.SetBool(isShootingHash, false);
-        }
-       
-
-        if (!isWalkingBack && backPressed)
-        {
-            animator.SetBool(isWalkingBackHash, true);
-        }
-        if (isWalkingBack && !backPressed)
-        {
-            animator.SetBool(isWalkingBackHash, false);
+            animator.SetBool(RecoilHash, false);
         }
 
-        if (!isrunningBack && (backPressed && runPressed))
-        {
-            animator.SetBool(isRunningBackHash, true);
-        }
-        if (isrunningBack && (!backPressed || !runPressed))
-        {
-            animator.SetBool(isRunningBackHash, false);
-        }
+        // Movement logic
+        animator.SetBool(isWalkingHash, forwardPressed && !runPressed);
+        animator.SetBool(isRunningHash, forwardPressed && runPressed);
+        animator.SetBool(isWalkingBackHash, backPressed && !runPressed);
+        animator.SetBool(isRunningBackHash, backPressed && runPressed);
+        animator.SetBool(isRunningLeftHash, leftPressed);
+        animator.SetBool(isRunningRightHash, rightPressed);
 
-        if (!isWalking && forwardPressed)
+        IEnumerator ResetIsReloading()
         {
-            animator.SetBool(isWalkingHash, true);
-        }
-        if (isWalking && !forwardPressed)
-        {
-            animator.SetBool(isWalkingHash, false);
-        }
-
-        if (!isrunning && (forwardPressed && runPressed))
-        {
-            animator.SetBool(isRunningHash, true);
-        }
-        if (isrunning && (!forwardPressed || !runPressed))
-        {
-            animator.SetBool(isRunningHash, false);
-        }
-
-        if (isrunning && leftPressed)
-        {
-            animator.SetBool(isRunningLeftHash, true);
-        }
-        else
-        {
-            animator.SetBool(isRunningLeftHash, false);
-        }
-
-        if (isrunning && rightPressed)
-        {
-            animator.SetBool(isRunningRightHash, true);
-        }
-        else
-        {
-            animator.SetBool(isRunningRightHash, false);
+            yield return new WaitForSeconds(2); // Assuming the reload animation is 2 seconds long
+            animator.SetBool(isReloadingHash, false);
         }
     }
 }
