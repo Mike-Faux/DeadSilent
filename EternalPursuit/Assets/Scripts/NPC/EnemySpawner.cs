c43d377923a3d8a5bd89703a91df6bcf74dc3653
Payson Allen <137624600+NoxisNG@users.noreply.github.com>
2024-06-30 21:40:12 -0500

    Changed spawn delay and objective text

--- EternalPursuit/Assets/Scripts/NPC/EnemySpawner.cs
+++ EternalPursuit/Assets/Scripts/NPC/EnemySpawner.cs
@@ -7,7 +7,7 @@ public class EnemySpawner : MonoBehaviour
     public GameObject Spawning;
 
     public int spawnCount;
-    public int spawnDelay =5;
+    public int spawnDelay;
 
     public bool UsePatrolPoints;
     public List<PatrolWaypoint> PatrolPath;
